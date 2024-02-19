1a87f1c3122ec0f2035b589c3dc45022dd791357
LorisBrignatz <loris.brignatz@etud.univ-jfc.fr>
2024-02-19 15:52:58 +0100

    Page home

--- lib/home.dart
+++ lib/home.dart
@@ -9,6 +9,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List titleProducts = [];
+
+  @override
+  void initState(){
+    titleProducts.add('Todos');
+    titleProducts.add('Combos');
+    titleProducts.add('Clasicos');
+    titleProducts.add('Addcionales');
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -22,7 +32,7 @@ class _HomeState extends State<Home> {
             width: size.width * 0.1,
           ),
         ),
-        iconTheme: IconThemeData(color: con.blanco), // Setting icon color
+        iconTheme: IconThemeData(color: con.blanco),
         actions: [
           IconButton(
             onPressed: () {},
@@ -34,9 +44,140 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      drawer: DrawerWidget(), // Corrected class name
+      drawer: DrawerWidget(),
       body: Stack(
-        children: [],
+        children: [
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Qué hay de cenar ?',
+                    style: TextStyle(
+                      color: con.amarillo,
+                      fontSize: 24,
+                      letterSpacing: 1.5
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 50,
+                width: size.width,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  physics: const ScrollPhysics(),
+                  itemCount: titleProducts.length,
+                  itemBuilder: (BuildContext context, int index){
+                    return Text(titleProducts[index], style: TextStyle(color: con.blanco, fontSize: 10),);
+                  },
+                ),
+              ),
+              listHomeProduct(size: size)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class listHomeProduct extends StatelessWidget {
+  const listHomeProduct({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: con.naranja,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  flex: 6,
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: con.blanco,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset("imagenes/Welcome 2.png",
+                          width: size.width * 0.15,),
+                        Text(
+                          'Hamburguesa',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                        Text(
+                          'Nuevo estilo',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 15),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(Icons.star, color: Colors.yellowAccent, size: 14,),
+                            Icon(Icons.heart_broken_outlined, size: 14,)
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(width: 15),
+                Expanded(
+                  flex: 6,
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: con.blanco,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset("imagenes/Welcome 2.png",
+                          width: size.width * 0.15,),
+                        Text(
+                          'Hamburguesa',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        Text(
+                          'Nuevo estilo',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 15),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(Icons.star, color: Colors.yellowAccent, size: 14,),
+                            Icon(Icons.heart_broken_outlined, size: 14,)
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
